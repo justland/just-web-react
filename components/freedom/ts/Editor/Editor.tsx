ce8eaa7d44376989eeb89a11d1bbb7bb23a182d7
unional <homawong@gmail.com>
2022-11-25 18:37:12 -0800

    feat: support ESM

--- components/freedom/ts/Editor/Editor.tsx
+++ components/freedom/ts/Editor/Editor.tsx
@@ -6,11 +6,21 @@ export function Editor() {
     <Tab.Group>
       <Tab.List>
         <Tab as={Fragment}>
-
+          <div role="tab" onClick={() => console.log('outer')}>
+            text
+            <div
+              onClick={(e) => {
+                e.preventDefault()
+                console.log('closing')
+              }}
+            >
+              [x]
+            </div>
+          </div>
         </Tab>
       </Tab.List>
       <Tab.Panels>
-
+        <Tab.Panel>hello</Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
   )
---
+++ libraries/react/.storybook/test-runner.cjs
@@ -0,0 +1,22 @@
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot')
+
+module.exports = {
+  setup() {
+    const toMatchImageSnapshot = configureToMatchImageSnapshot({
+      comparisonMethod: 'ssim',
+      failureThreshold: 0.01,
+      failureThresholdType: 'percent',
+      noColors: true
+    })
+    expect.extend({ toMatchImageSnapshot });
+  },
+  async postRender(page, context) {
+    if (!/-skip-snap$/.test(context.id)) {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({
+        customSnapshotsDir: `${process.cwd()}/__snapshots__/${process.platform}`,
+        customSnapshotIdentifier: context.id,
+      });
+    }
+  },
+};
