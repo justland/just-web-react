92dace2ff34edd19e03c58e7df6d9f51b60c0b51
unional <homawong@gmail.com>
2023-04-20 00:18:45 -0700

    chore: update storybook for react

--- libraries/react/.storybook/main.cjs
+++ libraries/react/.storybook/main.cjs
@@ -1,21 +1,18 @@
 module.exports = {
-	stories: ['../ts/**/*.stories.mdx', '../ts/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-storysource',
-		'storybook-dark-mode'
-	],
-	framework: '@storybook/react',
-	core: {
-		builder: '@storybook/builder-vite'
-	},
-	features: {
-		storyStoreV7: true,
-		interactionsDebugger: true
-	},
-	typescript: {
-		check: false
-	}
-}
+  stories: ['../ts/**/*.stories.mdx', '../ts/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-storysource', 'storybook-dark-mode', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  features: {
+    storyStoreV7: true,
+    interactionsDebugger: true
+  },
+  typescript: {
+    check: false
+  },
+  docs: {
+    autodocs: true
+  }
+};
