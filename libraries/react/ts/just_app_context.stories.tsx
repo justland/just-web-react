cf7922139d1e9514ab1b82dcf62fe085fdce857c
unional <homawong@gmail.com>
2023-04-21 00:42:15 -0700

    chore: add JustReactApp
    createJustAppContext.Provider will wrap root context

--- libraries/react/ts/just_app_context.stories.tsx
+++ libraries/react/ts/just_app_context.stories.tsx
@@ -1,9 +1,8 @@
-import { JustApp } from '@just-web/app'
 import type { Meta, StoryObj } from '@storybook/react'
 import { JustAppProvider } from './just_app_context.js'
-import { ReactGizmo } from './react_gizmo.js'
+import { App1Context, App1Info, activate as app1Activate } from './testing/app1.js'
+import { App2Context, App2Info, activate as app2Activate } from './testing/app2.js'
 import { AppInfo } from './testing/app_info.js'
-import { activate } from './testing/app1.js'
 
 const meta: Meta<typeof JustAppProvider> = {
 	component: JustAppProvider
@@ -16,8 +15,8 @@ type Story = StoryObj<typeof JustAppProvider>
 export const PropsVsContext: Story = {
 	loaders: [
 		async () => ({
-			app1: await activate(),
-			app2: await activate({ name: 'app2' })
+			app1: await app1Activate(),
+			app2: await app1Activate({ name: 'app1-dup' })
 		})
 	],
 	render(_, { loaded: { app1, app2 } }) {
@@ -30,6 +29,29 @@ export const PropsVsContext: Story = {
 					</div>
 				</JustAppProvider>
 			</JustAppProvider>
+		)
+	}
+}
+
+export const AppContext: Story = {
+	name: 'App specific context',
+	loaders: [
+		async () => ({
+			app1: await app1Activate(),
+			app2: await app2Activate()
+		})
+	],
+	render(_, { loaded: { app1, app2 } }) {
+		return (
+			<App1Context.Provider value={app1}>
+				<App2Context.Provider value={app2}>
+					<div className="flex gap-2">
+						<AppInfo title="App through context" />
+						<App1Info />
+						<App2Info />
+					</div>
+				</App2Context.Provider>
+			</App1Context.Provider>
 		)
 	}
 }
--- libraries/react/ts/testing/app2.tsx
+++ libraries/react/ts/testing/app2.tsx
@@ -1,7 +1,7 @@
 import type { JustApp } from '@just-web/app'
 import { justTestApp } from '@just-web/app/testing'
 import { createJustAppContext } from '../just_app_context.js'
-import { reactGizmo, type ReactGizmo } from '../react_gizmo.js'
+import { ReactGizmo, reactGizmo } from '../react_gizmo.js'
 import { valueGizmoFn, type ValueGizmo } from './value_gizmo.js'
 import { useContext } from 'react'
 import { mapKey } from 'type-plus'
@@ -11,30 +11,30 @@ import { tersify } from 'tersify'
  * Define the app type based on what the app needs,
  * and how it is composed in runtime.
  */
-export type JustApp1 = JustApp & ReactGizmo & ValueGizmo<number>
+export type JustApp2 = JustApp & ReactGizmo & ValueGizmo<number>
 
-export const App1Context = createJustAppContext<JustApp1>()
+export const App2Context = createJustAppContext<JustApp2>()
 
-export async function activate({ name }: { name: string } = { name: 'app1' }) {
+export async function activate({ name }: { name: string } = { name: 'app2' }) {
 	// note that you can also compose one app from another app.
 	const app = await justTestApp({ name })
-		.with(valueGizmoFn({ value: 100 }))
+		.with(valueGizmoFn({ value: 50 }))
 		.with(reactGizmo)
 		.create()
 
 	app.react.providers.register(({ children }) => (
-		<App1Context.Provider value={app}>{children}</App1Context.Provider>
+		<App2Context.Provider value={app}>{children}</App2Context.Provider>
 	))
 
 	return app
 }
 
-export function App1Info() {
-	const app = useContext(App1Context)
+export function App2Info() {
+	const app = useContext(App2Context)
 	return (
 		<div className="bg-slate-300 rounded-md p-3">
-			<p className="text-xl">App 1 Info</p>
-			<p>The app 1 contains the following info:</p>
+			<p className="text-xl">App 2 Info</p>
+			<p>The app 2 contains the following info:</p>
 			{mapKey(app, key => (
 				<p>
 					{key}: {tersify(app![key], { maxLength: 50 })}
