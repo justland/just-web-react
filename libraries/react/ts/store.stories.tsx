efaeff5f205104b966ed0d030808cfd5aea57d34
unional <homawong@gmail.com>
2023-04-21 23:34:14 -0700

    test: update store stories

--- libraries/react/ts/store.stories.tsx
+++ libraries/react/ts/store.stories.tsx
@@ -1,119 +1,8 @@
-import { createStore, Store } from '@just-web/states'
-import { FC, PropsWithChildren, useEffect, useState, VFC } from 'react'
+import { createStore, type Store } from '@just-web/states'
+import type { StoryObj } from '@storybook/react'
+import { useEffect, useState, type PropsWithChildren } from 'react'
 import { useStore } from './store.js'
 import { Card } from './testing/card.js'
-import { StoryObj } from '@storybook/react'
-
-function WithUpdater({
-	name = 'Using updater in `useStore()`',
-	store,
-	children
-}: PropsWithChildren<{ name?: string; store: Store<{ counter: number }> }>) {
-	const [updateValue, setUpdateValue] = useState<{ count: number; value: number }>({ count: 0, value: 0 })
-
-	const [value, setValue] = useStore(
-		store,
-		s => s.counter,
-		(s, v) => {
-			s.counter = v
-			setUpdateValue(u => ({ count: u.count + 1, value: v }))
-		}
-	)
-	console.count(`${name} render`)
-	return (
-		<Card>
-			<p>{name}</p>
-			<p>
-				<code>store.get().counter</code>: {store.get().counter}
-			</p>
-			<p>
-				<code>const [value] = useStore(...); console.log(value)</code>: {value}
-			</p>
-			<p>
-				<code>useStore(,, updater)</code> receives: {updateValue.value} (count: {updateValue.count})
-			</p>
-			<button
-				className="rounded bg-slate-500 p-2"
-				onClick={() =>
-					setValue(v => {
-						console.info(`${name} onClick.setValue`, v, v + 1)
-						return v + 1
-					})
-				}
-			>
-				{name} setValue
-			</button>
-			{children}
-		</Card>
-	)
-}
-
-const UseStore: FC<{ name?: string; store: Store<{ counter: number }> }> = ({
-	name = 'UseStore',
-	store,
-	children
-}) => {
-	const [value, setValue] = useStore(
-		store,
-		s => s.counter,
-		(s, v) => {
-			console.info(`${name} useStore.update`, s.counter, v)
-			s.counter = v
-		}
-	)
-
-	console.count(`${name} render`)
-	return (
-		<>
-			<div>
-				{name} state value: {value}
-			</div>
-			<div>
-				{name} store value: {store.get().counter}
-			</div>
-			<button
-				onClick={() =>
-					setValue(v => {
-						console.info(`${name} onClick.setValue`, v, v + 1)
-						return v + 1
-					})
-				}
-			>
-				{name} setValue
-			</button>
-			{children}
-		</>
-	)
-}
-
-function WithUseEffect({ store }: { store: Store<{ counter: number }> }) {
-	const [value, setValue] = useStore(store, s => s.counter)
-	useEffect(() => {
-		store.set(s => {
-			console.info('useEffect store.set:', s.counter, value)
-			s.counter = value
-		})
-	}, [value])
-
-	console.count('render')
-	return (
-		<Card>
-			<p>
-				<code>store.get().counter</code>: {store.get().counter}
-				<p>
-					This value is lack behind by one when using `setValue`, because change occurs within `useEffect()`
-					and the store value is not tracked by React
-				</p>
-			</p>
-			<p>
-				<code>const [value] = useStore(...); console.log(value)</code>: {value}
-			</p>
-			<button className="rounded bg-slate-500 p-2" onClick={() => setValue(v => v + 1)}>
-				Invoke setValue
-			</button>
-		</Card>
-	)
-}
 
 export default {
 	component: useStore
@@ -144,34 +33,174 @@ export const UpdateWithUseEffect: Story = {
 	render: () => <WithUseEffect store={createStore({ counter: 0 })} />
 }
 
-const Child = ({ store }: { store: Store<{ counter: number }> }) => {
+function WithUseEffect({ store }: { store: Store<{ counter: number }> }) {
+	const [value, setValue] = useStore(store, s => s.counter)
+	useEffect(() => {
+		store.set(s => {
+			console.info('useEffect store.set:', s.counter, value)
+			s.counter = value
+		})
+	}, [value])
+
+	console.count('render')
+	return (
+		<Card>
+			<p>
+				<code>store.get().counter</code>: {store.get().counter}
+				<p>
+					This value is lack behind by one when using `setValue`, because change occurs within `useEffect()`
+					and the store value is not tracked by React
+				</p>
+			</p>
+			<p>
+				<code>const [value] = useStore(...); console.log(value)</code>: {value}
+			</p>
+			<button className="button" onClick={() => setValue(v => v + 1)}>
+				Invoke setValue
+			</button>
+		</Card>
+	)
+}
+
+export const ParentVsChild = () => {
+	return <Parent store={createStore({ counter: 0 })} />
+}
+
+export const UpdateFromSibling = () => {
+	const store = createStore({ counter: 0 })
+	return (
+		<div className="flex flex-col gap-2">
+			<WithUpdater name="parent" store={store}>
+				<WithUpdater name="child" store={store} />
+			</WithUpdater>
+			<Card>
+				<p>Updating from sibling</p>
+				<ToggleStoreUpdate store={store} />
+			</Card>
+		</div>
+	)
+}
+
+function ToggleStoreUpdate({ store }: { store: Store<{ counter: number }> }) {
+	const [timer, setTimer] = useState(false)
+	// const [timerId, setTimerId] = useState<any>()
+	useEffect(() => {
+		if (timer) {
+			const id = setInterval(
+				() =>
+					store.set(s => {
+						console.info(`timer store.set:`, s.counter, s.counter + 1)
+						s.counter = s.counter + 1
+					}),
+				1000
+			)
+			return () => clearInterval(id)
+		}
+	}, [timer])
+	return (
+		<Card>
+			<button className="button" onClick={() => setTimer(v => !v)}>
+				{timer ? 'stop store update' : 'start store update'}
+			</button>
+		</Card>
+	)
+}
+
+export const StoreChangeTriggersRender: Story = {
+	decorators: [
+		Story => (
+			<div className="flex flex-col gap-2">
+				<Card>
+					<p>
+						This story has an <code>setInterval</code> of 0.3 second in a <code>useEffect</code>
+					</p>
+					<p>
+						It demonstrates that when the store is updated outside, the component with <code>useStore</code>{' '}
+						will be re-rendered.
+					</p>
+				</Card>
+
+				<Story />
+			</div>
+		)
+	],
+	render: () => {
+		const store = createStore({ counter: 0 })
+		const values: number[] = []
+
+		useEffect(() => {
+			const id = setInterval(
+				() =>
+					store.set(s => {
+						s.counter++
+					}),
+				300
+			)
+			return () => clearInterval(id)
+		}, [])
+		return <Counter store={store} values={values} />
+	}
+}
+
+function Counter({ store, values }: { store: Store<{ counter: number }>; values: number[] }) {
+	const [value] = useStore(store, s => s.counter)
+	values.push(value)
+	return (
+		<Card>
+			<p>
+				<code>const [value] = useStore(...); console.log(value)</code>: {value}
+			</p>
+			Render count:
+			<div id="values">{values}</div>
+		</Card>
+	)
+}
+
+function WithUpdater({
+	name = 'Using updater in `useStore()`',
+	store,
+	children
+}: PropsWithChildren<{ name?: string; store: Store<{ counter: number }> }>) {
+	const [updateValue, setUpdateValue] = useState<{ count: number; value: number }>({ count: 0, value: 0 })
+
 	const [value, setValue] = useStore(
 		store,
 		s => s.counter,
 		(s, v) => {
 			s.counter = v
+			setUpdateValue(u => ({ count: u.count + 1, value: v }))
 		}
 	)
-	console.count('re-render child')
-
+	console.count(`${name} render`)
 	return (
-		<>
-			<div>child.value: {value}</div>
-			<div>child.store: {store.get().counter}</div>
+		<Card>
+			<p>{name}</p>
+			<p>
+				<code>store.get().counter</code>: {store.get().counter}
+			</p>
+			<p>
+				<code>const [value] = useStore(...); console.log(value)</code>: {value}
+			</p>
+			<p>
+				<code>useStore(,, updater)</code> receives: {updateValue.value} (count: {updateValue.count})
+			</p>
 			<button
+				className="button"
 				onClick={() =>
 					setValue(v => {
-						console.info('child.setValue', v, '->', v + 1)
+						console.info(`${name} onClick.setValue`, v, v + 1)
 						return v + 1
 					})
 				}
 			>
-				child increment
+				invoke setValue
 			</button>
-		</>
+			{children}
+		</Card>
 	)
 }
-const Parent = ({ store }: { store: Store<{ counter: number }> }) => {
+
+function Parent({ store }: { store: Store<{ counter: number }> }) {
 	const [value, setValue] = useStore(
 		store,
 		s => s.counter,
@@ -201,85 +230,55 @@ const Parent = ({ store }: { store: Store<{ counter: number }> }) => {
 	}, [timer])
 
 	return (
-		<>
-			<button onClick={() => setTimer(v => !v)}>{timer ? 'stop store update' : 'start store update'}</button>
+		<Card className="flex flex-col gap-1 bg-slate-300">
 			<div>parent.value: {value}</div>
 			<div>parent.store: {store.get().counter}</div>
+			<div className="flex gap-1">
+				<button className="button" onClick={() => setTimer(v => !v)}>
+					{timer ? 'stop store update' : 'start store update'}
+				</button>
+				<button
+					className="button"
+					onClick={() =>
+						setValue(v => {
+							console.info('parent.setValue', v, '->', v + 1)
+							return v + 1
+						})
+					}
+				>
+					parent increment
+				</button>
+			</div>
+			<Child store={store} />
+		</Card>
+	)
+}
+
+function Child({ store }: { store: Store<{ counter: number }> }) {
+	const [value, setValue] = useStore(
+		store,
+		s => s.counter,
+		(s, v) => {
+			s.counter = v
+		}
+	)
+	console.count('re-render child')
+
+	return (
+		<Card className="bg-slate-400">
+			<div>child.value: {value}</div>
+			<div>child.store: {store.get().counter}</div>
 			<button
+				className="button"
 				onClick={() =>
 					setValue(v => {
-						console.info('parent.setValue', v, '->', v + 1)
+						console.info('child.setValue', v, '->', v + 1)
 						return v + 1
 					})
 				}
 			>
-				parent increment
+				child increment
 			</button>
-			<Child store={store} />
-		</>
+		</Card>
 	)
-}
-
-export const ParentChild_1 = () => {
-	return <Parent store={createStore({ counter: 0 })} />
-}
-const ToggleStoreUpdate = ({ store }: { store: Store<{ counter: number }> }) => {
-	const [timer, setTimer] = useState(false)
-	// const [timerId, setTimerId] = useState<any>()
-	useEffect(() => {
-		if (timer) {
-			const id = setInterval(
-				() =>
-					store.set(s => {
-						console.info(`timer store.set:`, s.counter, s.counter + 1)
-						s.counter = s.counter + 1
-					}),
-				1000
-			)
-			return () => clearInterval(id)
-		}
-	}, [timer])
-	return (
-		<div>
-			<button onClick={() => setTimer(v => !v)}>{timer ? 'stop store update' : 'start store update'}</button>
-		</div>
-	)
-}
-export const ParentChild_2 = () => {
-	const store = createStore({ counter: 0 })
-	return (
-		<>
-			<UseStore name="parent" store={store}>
-				<UseStore name="child" store={store} />
-			</UseStore>
-			<ToggleStoreUpdate store={store} />
-		</>
-	)
-}
-
-export const StoreChangeTriggerRender = () => {
-	const store = createStore({ counter: 0 })
-	const values: number[] = []
-	const Counter = () => {
-		const [value] = useStore(store, s => s.counter)
-		values.push(value)
-		return (
-			<>
-				<div>{value}</div>
-				<div id="values">{values}</div>
-			</>
-		)
-	}
-
-	useEffect(() => {
-		const id = setInterval(
-			() =>
-				store.set(s => {
-					s.counter++
-				}),
-			300
-		)
-		return () => clearInterval(id)
-	}, [])
-	return <Counter />
 }
