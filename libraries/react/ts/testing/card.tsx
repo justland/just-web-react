c2a06e95ca16eb950641c731d7700032fc87da31
unional <homawong@gmail.com>
2023-04-21 23:34:14 -0700

    test: update store stories

--- libraries/react/ts/testing/card.tsx
+++ libraries/react/ts/testing/card.tsx
@@ -1,5 +1,6 @@
-import type { PropsWithChildren } from 'react'
+import type { HtmlHTMLAttributes, PropsWithChildren } from 'react'
 
-export function Card({ children }: PropsWithChildren<Record<string, unknown>>) {
-	return <div className="bg-slate-300 rounded-md p-3">{children}</div>
+export function Card(props: PropsWithChildren<HtmlHTMLAttributes<any>>) {
+	const { className, ...rest } = props
+	return <div className={`rounded-md p-3 ${className ?? 'bg-slate-300'}`} {...rest}></div>
 }
