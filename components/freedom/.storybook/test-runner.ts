89f52efcc7f200723a4aebe15f2779f18a65a639
unional <homawong@gmail.com>
2023-08-07 21:31:42 -0700

    chore: try different snapshot config

--- components/freedom/.storybook/test-runner.ts
+++ components/freedom/.storybook/test-runner.ts
@@ -7,11 +7,8 @@ export async function postRender(page, context) {
 	if (!/-skip-snap$/.test(context.id)) {
 		const image = await page.screenshot()
 		expect(image).toMatchImageSnapshot({
-			blur: 1,
-			customDiffConfig: { threshold: 0.08 },
 			customSnapshotsDir: `${process.cwd()}/__snapshots__/${process.platform}`,
 			customSnapshotIdentifier: context.id,
-			failureThresholdType: 'percent',
 		})
 	}
 }
