8bb95559ec2185294dc47b6c9762c43fc5faf823
unional <homawong@gmail.com>
2023-08-07 21:23:26 -0700

    chore: adjust snapshot config

--- components/freedom/.storybook/test-runner.ts
+++ components/freedom/.storybook/test-runner.ts
@@ -1,27 +1,17 @@
-import { configureToMatchImageSnapshot } from 'jest-image-snapshot'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
 export function setup() {
-	const toMatchImageSnapshot = configureToMatchImageSnapshot({
-		// ssim config
-		// comparisonMethod: 'ssim',
-
-		// pixel config
-		comparisonMethod: 'pixelmatch',
-		blur: 1,
-		customDiffConfig: { threshold: 0.08 },
-
-		failureThresholdType: 'percent',
-		customSnapshotsDir: `${process.cwd()}/__snapshots__/${process.platform}`,
-		noColors: true
-	})
 	expect.extend({ toMatchImageSnapshot })
 }
 export async function postRender(page, context) {
 	if (!/-skip-snap$/.test(context.id)) {
 		const image = await page.screenshot()
 		expect(image).toMatchImageSnapshot({
+			blur: 1,
+			customDiffConfig: { threshold: 0.08 },
 			customSnapshotsDir: `${process.cwd()}/__snapshots__/${process.platform}`,
-			customSnapshotIdentifier: context.id
+			customSnapshotIdentifier: context.id,
+			failureThresholdType: 'percent',
 		})
 	}
 }
