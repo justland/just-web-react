be6800f375f0bccfb0b831b00e91ed05590bab16
unional <homawong@gmail.com>
2023-08-13 13:25:41 -0700

    chore: add onKeyDown

--- components/freedom/ts/terminal/terminal2.tsx
+++ components/freedom/ts/terminal/terminal2.tsx
@@ -1,23 +1,30 @@
 import {
+	createContext,
 	forwardRef,
+	memo,
+	useContext,
+	useEffect,
 	useImperativeHandle,
 	useRef,
 	type ChangeEventHandler,
-	useEffect,
-	createContext,
-	useContext,
-	memo
+	type KeyboardEventHandler,
+	type RefObject,
+	type ReactNode
 } from 'react'
 
 export type TerminalWidgetContextProps = {
+	output: Array<ReactNode>
 	onChange?: ChangeEventHandler<HTMLInputElement> | undefined
-	inputRef?: React.RefObject<HTMLInputElement>
+	onKeyDown?: KeyboardEventHandler<HTMLInputElement> | undefined
+	inputRef?: RefObject<HTMLInputElement> | undefined
 }
 
 export const TerminalWidgetContext = createContext<TerminalWidgetContextProps>(null as any)
 
 export interface TerminalProps {
 	onChange?: ChangeEventHandler<HTMLInputElement> | undefined
+	onKeyDown?: KeyboardEventHandler<HTMLInputElement> | undefined
+	output: Array<ReactNode>
 }
 
 export const TerminalWidget = memo(
@@ -29,29 +36,44 @@ export const TerminalWidget = memo(
 			}
 		}))
 
-		// useEffect(() => {
-		// 	if (!inputRef.current) return
-		// 	inputRef.current.focus()
-		// }, [inputRef])
+		useEffect(() => {
+			if (!inputRef.current) return
+			inputRef.current.focus()
+		}, [inputRef])
 		return (
-			// <TerminalWidgetContext.Provider value={{ onChange: props.onChange, inputRef }}>
-				<div>
-					<div>output</div>
+			<TerminalWidgetContext.Provider value={{ ...props, inputRef }}>
+				<>
+					<TerminalOutput />
 					<div>
 						<div>
-							$ <input onChange={props.onChange} ref={inputRef} />
+							$ <TerminalInput />
 						</div>
 					</div>
-				</div>
-			// </TerminalWidgetContext.Provider>
+				</>
+			</TerminalWidgetContext.Provider>
 		)
 	})
 )
-
-export function TerminalInput() {
-	const { onChange, inputRef } = useContext(TerminalWidgetContext)
-
-	return <input onChange={onChange} ref={inputRef} />
+export interface TerminalOutputProps {
+	className?: string
+	children?: ReactNode | ((bag: { output: Array<ReactNode> }) => ReactNode)
 }
 
-export const Terminal = Object.assign(TerminalWidget, {})
+export function TerminalOutput({ className, children }: TerminalOutputProps) {
+	const { output } = useContext(TerminalWidgetContext)
+	const resolvedChildren = typeof children === 'function' ? children({ output }) : children
+	return (
+		<div className={className}>{resolvedChildren || output.map((line, i) => <div key={i}>{line}</div>)}</div>
+	)
+}
+
+export function TerminalInput() {
+	const { inputRef, onChange, onKeyDown } = useContext(TerminalWidgetContext)
+
+	return <input onChange={onChange} onKeyDown={onKeyDown} ref={inputRef} />
+}
+
+export const Terminal = Object.assign(TerminalWidget, {
+	Widget: TerminalWidget,
+	Input: TerminalInput
+})
