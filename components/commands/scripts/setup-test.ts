91cd66fa1c7c8297f7d16d5299cc47190f387e8c
unional <homawong@gmail.com>
2025-12-14 01:03:03 -0800

    chore: replace depcheck with knip

--- components/commands/scripts/setup-test.ts
+++
@@ -1,9 +0,0 @@
-// import matchers, { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
-// import { cleanup } from '@testing-library/react'
-// import { afterEach, expect } from 'vitest'
-
-// afterEach(cleanup)
-
-// expect.extend(matchers)
-
-import '@testing-library/jest-dom'
